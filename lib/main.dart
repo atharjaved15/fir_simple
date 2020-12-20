4649364ed4a391edfd760833a5c7ead7bb4c657f
Athar Javed <atharjaved15@gmail.com>
2020-12-19 22:20:03 -0800

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -62,6 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
+      //ali
       database.reference().child("details").set(
         {"Name" : "Athar Javed"},
       );
